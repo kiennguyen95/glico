bf0c2889ce4b77e4bbd145ec871c453b76903bb6
Kien Nguyen <kien.nguyentrung.95@gmail.com>
2019-11-19 00:58:19 +0700

    Update style

--- web/themes/custom/glico/js/glico.js
+++ web/themes/custom/glico/js/glico.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function ($) {
+  // Hamburger menu control
+  $("#btn-hamburger").click(function () {
+    $(".header-menu-bg").show();
+    $(".header-menu").slideDown();
+  });
+
+  $(".header-menu-bg, .header-menu li a").click(function () {
+    $(".header-menu-bg").hide();
+    $(".header-menu").slideUp();
+  });
+
   // Tabs control
   $("#part-info ul.tabs li").click(function () {
     $(this).addClass("is-active");
