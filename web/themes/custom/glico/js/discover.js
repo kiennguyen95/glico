41c3c880c02597d1d6beede300aa7cd67982af6b
nhtoan611 <nhtoan611@gmail.com>
2019-11-21 02:45:06 +0700

    start video page

--- web/themes/custom/glico/js/discover.js
+++ web/themes/custom/glico/js/discover.js
@@ -1,7 +1,19 @@
 (function ($) {
 
   $(document).ready(function ($) {
-
+    setBtnCommand();
   });
 
+  function setBtnCommand() {
+    $('#btn-myvideo').click(function () {
+      $('select[name="uid-current"] option:selected').removeAttr('selected');
+      $('select[name="uid-current"] option:nth-child(2)').attr('selected','selected');
+      // $('.views-exposed-form .form-actions .form-submit').click();
+    });
+    $('#btn-newvideo').click(function () {
+      $('select[name="uid-current"] option:selected').removeAttr('selected');
+      $('select[name="uid-current"] option:nth-child(1)').attr('selected','selected');
+      // $('.views-exposed-form .form-actions .form-submit').click();
+    });
+  };
 }(jQuery));
