be8c58d96eb6352c5e229a31b3cbbf4ea610849c
Do Xuan Tho <gonepas08@gmail.com>
2019-11-22 02:27:37 +0700

    edit css

--- web/modules/custom/glico_submission/src/Step/StepPreview.php
+++ web/modules/custom/glico_submission/src/Step/StepPreview.php
@@ -6,6 +6,7 @@ namespace Drupal\glico_submission\Step;
 use Drupal\file\Entity\File;
 use Drupal\glico_submission\Button\StepPreviewNextButton;
 use Drupal\glico_submission\Button\StepPreviewPreviousButton;
+use Drupal\user\Entity\User;
 
 /**
  * Class StepFinalize.
@@ -40,11 +41,15 @@ class StepPreview extends BaseStep {
     $frame = $tempstore->get('frame');
     $baby_name = $tempstore->get('baby_name');
     $caption = $tempstore->get('caption');
+    $user_img = User::load(\Drupal::currentUser()->id());
+    $user_img = $user_img->get('user_picture')->getValue();
+    $user_img = File::load($user_img[0]['target_id']);
     $form['preview'] = [
       '#theme' => 'glico_submission_preview',
       '#variables' => [
         'submitter' => [
           'name' => \Drupal::currentUser()->getDisplayName(),
+          'img_src' => $user_img == NULL ? NULL : file_create_url($user_img->getFileUri()),
         ],
         'title' => $baby_name,
         'caption' => $caption,
