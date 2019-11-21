d99797a4357ca004646f70ea7428a07a47498fe0
Nguyen Trung Kien <ntkien12@cmc.com.vn>
2019-11-21 15:24:13 +0700

    Share FB

--- web/themes/custom/glico/js/detail.js
+++ web/themes/custom/glico/js/detail.js
@@ -1,10 +1,22 @@
 (function ($) {
 
   $(document).ready(function ($) {
-    $(".info-wrapper .btn-share").click(function () {
-      console.log("aa");
-      console.log($(".fb-share-button"));
-      $(".fb-share-button").click();
+
+    $(".info-wrapper #btn-share").click(function() {
+      FB.ui({
+        method: 'share',
+        href: window.location.href,
+        hashtag: '#GlicoDance',
+        quote: 'quote',
+      }, function(response) {
+        // if(response && response.post_id){
+        //
+        // }
+        // else{}
+        // if (typeof response !== 'undefined') {
+        //   alert('Thanks for sharing');
+        // }
+      });
     });
   });
 
