de34a92c15523ad3d3294cd9cec4607f068c2395
Nguyen Trung Kien <ntkien12@cmc.com.vn>
2019-12-16 19:15:16 +0700

    Fix CSS modal form on iphone

--- web/themes/custom/glico/js/glico.js
+++ web/themes/custom/glico/js/glico.js
@@ -1,6 +1,13 @@
 (function ($) {
 
   $(document).ready(function ($) {
+    var ua = navigator.userAgent;
+    var iPhone = ua.indexOf('iPhone') > 0;
+
+    if (iPhone) {
+      $('body').addClass('on-iphone');
+    }
+
     // Hamburger menu control
     $("#btn-hamburger").click(function () {
       $(".header-menu-bg").show();
