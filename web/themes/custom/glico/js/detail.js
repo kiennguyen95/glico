501007e48ecbc6f53cc99b4e61833a9a9fe3b099
Kien Nguyen <kien.nguyentrung.95@gmail.com>
2019-12-18 22:26:34 +0700

    Fix JS in node detail

--- web/themes/custom/glico/js/detail.js
+++ web/themes/custom/glico/js/detail.js
@@ -13,6 +13,22 @@
       }
     }
 
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '748106385614500',
+        xfbml      : true,
+        version    : 'v2.3'
+      });
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/vi_VN/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     $(".info-wrapper #btn-share").click(function() {
       FB.ui({
         method: 'share',
